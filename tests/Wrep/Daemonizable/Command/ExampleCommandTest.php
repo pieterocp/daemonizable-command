0f220f3e1cd5058fd53814c5bd123c9b63a8a3c0
Pieter Oliver <pieter@care-planner.co.uk>
2024-01-17 17:36:55 +0000

    Fix test for Symfony 7 upgrade

--- tests/Wrep/Daemonizable/Command/ExampleCommandTest.php
+++ tests/Wrep/Daemonizable/Command/ExampleCommandTest.php
@@ -6,7 +6,6 @@ use Acme\DemoBundle\Command\ExampleCommand;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ExampleCommandTest extends TestCase
 {
@@ -24,10 +23,6 @@ class ExampleCommandTest extends TestCase
     public function testExecutionOnceCase(): void
     {
         $sut = new ExampleCommand();
-
-        // required otherwise the getContainer fails with a type error
-        $mockContainer = $this->createMock(ContainerInterface::class);
-        $sut->setContainer($mockContainer);
 
         $input = $this->createMock(InputInterface::class);
         $output = $this->createMock(OutputInterface::class);
